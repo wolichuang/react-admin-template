69fc0b9adf4d5d5966352e67941fde7b89457edc
lc123.hi@163.com <lc123.hi@163.com>
2021-05-28 15:09:34 +0800

    deploy

---
+++ precache-manifest.761d7073eb32fd0f44b7451bd94755f3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b00981e51a41e3024c3267c741f079",
+    "url": "/react-admin-template/index.html"
+  },
+  {
+    "revision": "5d897baea5bf5accca43",
+    "url": "/react-admin-template/static/css/0.aa7e269e.chunk.css"
+  },
+  {
+    "revision": "b19fdcff04c28766aa6a",
+    "url": "/react-admin-template/static/css/7.070bbdd7.chunk.css"
+  },
+  {
+    "revision": "f8e452953bc3b06a6667",
+    "url": "/react-admin-template/static/css/8.c46b373d.chunk.css"
+  },
+  {
+    "revision": "fadd1dcc2c44cf9fbf7a",
+    "url": "/react-admin-template/static/css/Dashboard.ba3450a7.chunk.css"
+  },
+  {
+    "revision": "699aebf74dd9e894c51c",
+    "url": "/react-admin-template/static/css/Error404.b27f81ed.chunk.css"
+  },
+  {
+    "revision": "11b8f8dd32952ce42a88",
+    "url": "/react-admin-template/static/css/Orders.7dfdfac9.chunk.css"
+  },
+  {
+    "revision": "e419fe370e85beed5488",
+    "url": "/react-admin-template/static/css/Publish.22dbc4ac.chunk.css"
+  },
+  {
+    "revision": "1bf54be27af693e6bdaa",
+    "url": "/react-admin-template/static/css/main.636fa31c.chunk.css"
+  },
+  {
+    "revision": "5d897baea5bf5accca43",
+    "url": "/react-admin-template/static/js/0.3266d4fa.chunk.js"
+  },
+  {
+    "revision": "b19fdcff04c28766aa6a",
+    "url": "/react-admin-template/static/js/7.24651cd6.chunk.js"
+  },
+  {
+    "revision": "40a34bec8bd32af7902df7abf348900a",
+    "url": "/react-admin-template/static/js/7.24651cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e452953bc3b06a6667",
+    "url": "/react-admin-template/static/js/8.f84708a1.chunk.js"
+  },
+  {
+    "revision": "fadd1dcc2c44cf9fbf7a",
+    "url": "/react-admin-template/static/js/Dashboard.50dbce1b.chunk.js"
+  },
+  {
+    "revision": "699aebf74dd9e894c51c",
+    "url": "/react-admin-template/static/js/Error404.948b8a60.chunk.js"
+  },
+  {
+    "revision": "11b8f8dd32952ce42a88",
+    "url": "/react-admin-template/static/js/Orders.801b633a.chunk.js"
+  },
+  {
+    "revision": "e419fe370e85beed5488",
+    "url": "/react-admin-template/static/js/Publish.cabe4767.chunk.js"
+  },
+  {
+    "revision": "1bf54be27af693e6bdaa",
+    "url": "/react-admin-template/static/js/main.9bef8e5f.chunk.js"
+  },
+  {
+    "revision": "04514fde0362dcc71328",
+    "url": "/react-admin-template/static/js/runtime-main.3186c372.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/react-admin-template/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "004d31d071928f152d025548f816f25b",
+    "url": "/react-admin-template/static/media/bg.004d31d0.png"
+  },
+  {
+    "revision": "8a2cbdbbe650ebf89f8329a27eb2ab09",
+    "url": "/react-admin-template/static/media/sider.8a2cbdbb.png"
+  }
+]);
